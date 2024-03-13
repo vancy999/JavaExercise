5fdcdce18059f8a37b4b34a043cb3075b1801f35
Frfusch21 <reviewfarmei20@gmail.com>
2024-03-13 09:41:52 +0700

    AutomationAdvancedExercise_Selenium

---
+++ src/test/java/com/automationexercise/tests/TestBasic.java
@@ -0,0 +1,32 @@
+package com.automationexercise.tests;
+
+import com.automationexercise.utils.BrowserManager;
+import com.automationexercise.utils.PropertiesLoader;
+import org.openqa.selenium.WebDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+
+import java.io.IOException;
+
+public class TestBasic {
+
+    protected static ThreadLocal<WebDriver> tdriver = new ThreadLocal<>();
+
+    public static synchronized WebDriver getDriver() {
+        return tdriver.get();
+    }
+
+    @BeforeMethod
+    public void setup() throws IOException {
+        String url = PropertiesLoader.loadProperty("url");
+        WebDriver driver = BrowserManager.doBrowserSetup();
+        tdriver.set(driver);
+        getDriver().get(url);
+    }
+
+    @AfterMethod
+    public void tearDown() {
+        getDriver().quit();
+        tdriver.remove();
+    }
+}
